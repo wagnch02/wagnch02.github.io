e0efd4342299957c73cf0df2857a6c28f6792e84
wagnch02 <wagner.chris25@gmail.com>
2014-02-27 12:23:38 -0600

    added factors

---
+++ jshello.js
@@ -0,0 +1,26 @@
+/* This is a comment */
+
+hello = function() {
+    var newh1 = document.createElement('h1')
+    newh1.innerHTML = 'Hello World!'
+    document.getElementById("temp1").appendChild(newh1)
+    var newp = document.createElement('p')
+    document.getElementById("count").appendChild(newp)
+    //document.body.appendChild(newh1)
+}
+// this is a single line comment
+myCounter = 0
+factors = function() {
+    var newp = document.getElementById("count");
+    myCounter = myCounter +1;    
+    newp.innerHTML = myCounter
+    newp.style.fontSize = myCounter+"px";
+    if (newp.style.color=="blue") {
+        newp.style.color="red";
+    }
+    else {
+        newp.style.color="blue";
+    }
+}
+
+window.onload = hello

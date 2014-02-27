7709c91e804452ebc7d015a21f517369aa8dacdd
wagnch02 <wagner.chris25@gmail.com>
2014-02-27 12:43:33 -0600

    ST

--- jshello.js
+++ jshello.js
@@ -1,20 +1,20 @@
 /* This is a comment */
 
 hello = function() {
-    var newh1 = document.createElement('h1')
-    newh1.innerHTML = 'Hello World!'
+    //var newh1 = document.createElement('h1')
+    //newh1.innerHTML = 'Hello World!'
     document.getElementById("temp1").appendChild(newh1)
-    var newp = document.createElement('p')
-    document.getElementById("count").appendChild(newp)
     //document.body.appendChild(newh1)
 }
 // this is a single line comment
 myCounter = 0
 factors = function() {
-    var newp = document.getElementById("count");
-    myCounter = myCounter +1;    
+    var newp = document.createElement('p')
+    document.getElementById("count").appendChild(newp)
+    //var newp = document.getElementById("count");
+    //myCounter = myCounter +1;    
     newp.innerHTML = document.getElementById("myInt").value;
-    newp.style.fontSize = myCounter+"px";
+    //newp.style.fontSize = myCounter+"px";
     if (newp.style.color=="blue") {
         newp.style.color="red";
     }

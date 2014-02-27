71a017b3e05e4624d88f7bf3979f9904db1d7dc7
wagnch02 <wagner.chris25@gmail.com>
2014-02-27 13:19:13 -0600

    fixed error

--- jshello.js
+++ jshello.js
@@ -3,7 +3,7 @@
 hello = function() {
     //var newh1 = document.createElement('h1')
     //newh1.innerHTML = 'Hello World!'
-    document.getElementById("temp1").appendChild(newh1)
+    //document.getElementById("temp1").appendChild(newh1)
     //document.body.appendChild(newh1)
 }
 // this is a single line comment

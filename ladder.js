974d9fe86661be621db6f6f5a8b4cc3830e0ff84
wagnch02 <wagner.chris25@gmail.com>
2014-03-13 10:39:51 -0500

    added wordladder

---
+++ ladder.js
@@ -0,0 +1,53 @@
+/* This is a comment */
+
+hello = function() {
+    //var newh1 = document.createElement('h1')
+    //newh1.innerHTML = 'Hello World!'
+    //document.getElementById("temp1").appendChild(newh1)
+    //document.body.appendChild(newh1)
+}
+// this is a single line comment
+myCounter = 0
+factors = function() {
+    var newList = document.createElement('ul')
+    //var newp = document.getElementById("count");
+    //myCounter = myCounter +1;    
+	var myVal = document.getElementById("myInt").value;
+    //newItem.innerHTML = myVal;
+    //newItem.style.fontSize = "25px";
+    //newItem.style.color="red";
+	var ele = document.getElementById("count");
+	if (ele) {
+	   while (ele.firstChild) {
+	      ele.removeChild(ele.firstChild);
+	   }
+	}
+	
+	var start = 2;
+	var lastInt = 0;
+	while (myVal > 1) {
+	   while (myVal % start == 0) {
+	      
+	      //var newItem = document.createElement('li')
+          //document.getElementById("count").appendChild(newItem)
+		  myVal /= start;
+		  //newItem.innerHTML = start;
+		  if (start != lastInt) {
+		     insertItem(start, "item"+start);
+		  }
+		  lastInt = start;
+	   }
+	   start = start + 1;
+	}
+	
+}
+
+insertItem = function(value, newListItem) {
+   var ul = document.getElementById("count");
+   var li = document.createElement("li");
+   li.innerHTML = value;
+   ul.appendChild(li);
+}
+
+
+window.onload = hello
